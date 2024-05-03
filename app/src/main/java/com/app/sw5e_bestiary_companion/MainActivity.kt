80fd5fb5c3b824b8ae46193f5d05317196636902
jah83440 <har18043@byui.edu>
2024-05-03 08:35:38 -0600

    setup android studio

---
+++ app/src/main/java/com/app/sw5e_bestiary_companion/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.app.sw5e_bestiary_companion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.app.sw5e_bestiary_companion.ui.theme.SW5eBestiaryCompanionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SW5eBestiaryCompanionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SW5eBestiaryCompanionTheme {
+        Greeting("Android")
+    }
+}
