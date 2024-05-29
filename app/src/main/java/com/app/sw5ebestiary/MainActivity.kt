6c770dd6dac6621804fa19d1085b1edda6e01e16
jah83440 <har18043@byui.edu>
2024-05-28 19:00:21 -0600

    Color tweaks, removed fab component

--- app/src/main/java/com/app/sw5ebestiary/MainActivity.kt
+++ app/src/main/java/com/app/sw5ebestiary/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.app.sw5ebestiary
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -28,11 +27,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
